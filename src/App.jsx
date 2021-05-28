fc66586cf4ff904594661641ceed8bc9f5492c76
igorwhisp <incwhisper@gmail.com>
2021-05-28 11:21:22 +0300

    add material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import ButtonAppBar from './navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
+      <ButtonAppBar />
       <h1>Stephan</h1>
     </div>
   );
